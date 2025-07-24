4ee095ce346d83eb176a7629af0e89d5a60be493
Ariel Ramirez <arielramirez659@gmail.com>
2025-07-23 22:34:53 -0600

    Codigo Funcionando

--- EstudiantesSQL.py
+++ EstudiantesSQL.py
@@ -2,19 +2,19 @@ from ConexionSQL import *
 
 class Estudiantes:
 
+   
     def ingresarEstudiante(matricula, nombre, correo, contraseña):
+     try:
+        conexion = CConexion.ConexionBaseDeDatos()
+        cursor = conexion.cursor()
+        sql = "INSERT INTO Estudiante VALUES (%s, %s, %s, %s);"
+        valores = (matricula, nombre, correo, contraseña)
+        cursor.execute(sql, valores)
+        conexion.commit()
+        print(cursor.rowcount, "Estudiante registrado correctamente.")
+        conexion.close()
 
-        try:
-            conexion = CConexion.ConexionBaseDeDatos()
-            cursor = conexion.cursor()
-            sql = "INSERT INTO Estudiante VALUES (%s, %s, %s, %s);"
-
-            #La variable valores es una tupla que contiene los valores a insertar
-            valores = (matricula, nombre, correo, contraseña)
-            cursor.execute(sql, valores)
-            conexion.commit()
-            print(cursor.rowcount,"Estudiante registrado correctamente.")
-            conexion.close()
-
-        except mysql.connector.Error as error:
-            print("Error al insertar el estudiante: {}".format(error))
+     except mysql.connector.errors.IntegrityError as error:
+        raise Exception("La matrícula o el correo ya están registrados.")
+     except Exception as error:
+        raise Exception(f"Error inesperado al insertar: {error}")
